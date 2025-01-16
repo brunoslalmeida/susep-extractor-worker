543f8ac0601e3eae429e07f2b58b76b449168f79
Bruno de Souza Lobo Almeida <brunoslalmeida@gmail.com>
2025-01-16 01:34:15 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -18,10 +18,10 @@ export default {
 
 		if (request.method === 'GET') {
 			response = await getConfigs();
+		} else {
+			response = await getItems();
 		}
-
-		response = await getItems();
-
+		
 		response.headers.set('Access-Control-Allow-Origin', '*'); // Allow all origins (for testing, be specific in production)
 		response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allowed methods
 		response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
