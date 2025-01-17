97298208906db81bcad2ab8aba873ce3d949d374
Bruno de Souza Lobo Almeida <brunoslalmeida@gmail.com>
2025-01-16 21:06:16 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -21,10 +21,9 @@ export default {
 		} else {
 			response = await getItems();
 		}
-		
-		response.headers.set('Access-Control-Allow-Origin', '*'); // Allow all origins (for testing, be specific in production)
-		response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allowed methods
-		response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
+
+		// response.headers.set('Access-Control-Allow-Origin', '*'); // Allow all origins (for testing, be specific in production)
+		// response.headers.set('Access-Control-Allow-Methods', 'GET, POST'); // Allowed methods
 
 		return response;
 
