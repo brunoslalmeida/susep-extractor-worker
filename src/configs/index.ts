0c888359d6106376795732cc683da4894e51083e
bruno.s.l.almeida <bruno.s.l.almeida@gmail.com>
2026-01-29 02:24:29 +0000

    update index.ts

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,31 +1,28 @@
 import _resseguroConfig from './resseguro.json';
 import _seguroConfig from './seguro.json';
-import _demonstraticoConfig from './demonstrativo.json'
+import _demonstraticoConfig from './demonstrativo.json';
 
 export const resseguroConfig: ResseguroConfig = {
-  companies: _resseguroConfig.companies,
-  types: _resseguroConfig.types.map(type => ({
-	code: type.code as "22A" | "22P" | "23",
-    value: type.value,
-  }))
+	companies: _resseguroConfig.companies,
+	types: _resseguroConfig.types.map((type) => ({
+		code: type.code as '22A' | '22P' | '23',
+		value: type.value,
+	})),
 };
 
 export const seguroConfig: SeguroConfig = {
-    companies: _seguroConfig.companies,
-    types: _seguroConfig.types.map(type => ({
-      code: type.code as "22A" | "22P" | "23",
-      value: type.value,
-    }))
+	companies: _seguroConfig.companies,
+	types: _seguroConfig.types.map((type) => ({
+		code: type.code as '22A' | '22P' | '23',
+		value: type.value,
+	})),
 };
 
-const corrente = new Date().getFullYear(); // 2026
-const anoFinal = corrente - 1;
+const YEAR = 2011
+const length = Math.max(0, new Date().getFullYear() - YEAR);
 
 export const demonstracaoConfig: DemonstrativoConfig = {
-  entities: _demonstraticoConfig.entity,
-  types: _demonstraticoConfig.type,
-  years: Array.from(
-    { length: anoFinal - _demonstraticoConfig.year + 1 },
-    (_, i) => _demonstraticoConfig.year + i
-  )
-}
+	entities: _demonstraticoConfig.entity,
+	types: _demonstraticoConfig.type,
+	years: Array.from({ length }, (_, i) => YEAR + i),
+};
